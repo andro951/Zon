8c151d45c46c9aa2612e595d57ad98a8cfbb056d
andro951 <andro951@gmail.com>
2025-06-01 22:20:32 -0400

    fix this.rect -> this

--- assets/js/built/ui/uiStates/uiState.js
+++ assets/js/built/ui/uiStates/uiState.js
@@ -3,7 +3,7 @@
 Zon.UI.UIStateCanvas = class extends Zon.UI.UIElementCanvas {
     constructor(canvasId, animation, width = 300, height = 150, zIndex = 0, parent = Zon.device) {
         super(canvasId, width, height, zIndex, parent);
-        this.animation = typeof animation === 'function' ? animation(this.rect) : animation;
+        this.animation = typeof animation === 'function' ? animation(this) : animation;
         this.bindAll();
     }
 
