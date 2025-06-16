48717fb1530d2e353e683a4f05fb5e736a81b19a
andro951 <andro951@gmail.com>
2025-06-16 14:51:03 -0400

    Working on stageUIState

--- assets/js/built/ui/uiStates/stageUIState.js
+++ assets/js/built/ui/uiStates/stageUIState.js
@@ -8,14 +8,16 @@ Zon.UI.StageUIState = class extends Zon.UI.CloseButtonLinkedUIState {
     postConstructor() {
         super.postConstructor();
 
+        const stageSelectBorder = 2;
+        //Page label
         this.label = Zon.UI.UIElementDiv2.create('stageSelectLabel', Zon.UI.UIElementZID.CLOSE_BUTTON_MENU, this, {
             constructorFunc: (d) => {
                 d.element.style.color = Struct.Color.fromUInt(0xFFFFFFFF).cssString;
                 d.element.style.backgroundColor = Struct.Color.fromUInt(0x000000FF).cssString;
-                d.element.style.border = `2px solid #AAA`;
+                d.element.style.border = `${stageSelectBorder}px solid #AAA`;
                 d.element.style.borderRadius = `$8px`;
                 d.element.style.fontWeight = `bold`;
-                d.element.textContent = `Select Stage`;
+                d.element.textContent = `Stage Select`;
                 d.element.style.display = 'flex';
                 d.element.style.justifyContent = 'center';
                 d.element.style.alignItems = 'center';
@@ -24,10 +26,48 @@ Zon.UI.StageUIState = class extends Zon.UI.CloseButtonLinkedUIState {
                 d.element.style.userSelect = 'none';
             },
             setupFunc: (d) => {
-                d.replaceLeft(() => Zon.UI.stageUIState.width * 0.1);
-                d.replaceTop(() => Zon.UI.stageUIState.height * 0.02);
-                d.replaceWidth(() => Zon.UI.stageUIState.width - d.left * 2, { d });
-                d.replaceHeight(() => Zon.UI.stageUIState.height * 0.05);
+                d.replaceLeft(() => d.parent.width * 0.1, { d });
+                d.replaceTop(() => d.parent.height * 0.02, { d });
+                d.replaceWidth(() => d.parent.width - d.left * 2, { d });
+                d.replaceHeight(() => d.parent.height * 0.05, { d });
+            }
+        });
+
+        //Aether bonus and settings
+        this.aetherBonusAndSettingsContianer = Zon.UI.UIElementDiv2.create('aetherBonusAndSettingsContainer', Zon.UI.UIElementZID.CLOSE_BUTTON_MENU, this, {
+            // constructorFunc: (d) => {
+
+            // },
+            postConstructorFunc: (d) => {
+                const xPaddingMult = 0.005;
+                this.aetherBonus = Zon.UI.UIElementDiv2.create('aetherStageBonus', Zon.UI.UIElementZID.CLOSE_BUTTON_MENU, d, {
+                    constructorFunc: (d) => {
+                        d.element.style.backgroundColor = Struct.Color.fromUInt(0x00FF00FF).cssString;
+                    },
+                    setupFunc: (d) => {
+                        d.replaceLeft(() => 0, { d });
+                        d.replaceTop(() => 0, { d });
+                        d.replaceWidth(() => d.parent.width * (0.5 - xPaddingMult), { d });
+                        d.replaceHeight(() => d.parent.height, { d });
+                    }
+                });
+                this.stageSettings = Zon.UI.UIElementDiv2.create('stageSettings', Zon.UI.UIElementZID.CLOSE_BUTTON_MENU, d, {
+                    constructorFunc: (d) => {
+                        d.element.style.backgroundColor = Struct.Color.fromUInt(0x0000FFFF).cssString;
+                    },
+                    setupFunc: (d) => {
+                        d.replaceLeft(() => d.parent.width * (0.5 + xPaddingMult), { d });
+                        d.replaceTop(() => 0, { d });
+                        d.replaceWidth(() => d.parent.width * (0.5 - xPaddingMult), { d });
+                        d.replaceHeight(() => d.parent.height, { d });
+                    }
+                });
+            },
+            setupFunc: (d) => {
+                d.replaceLeft(() => d.parent.width * 0.1, { d });
+                d.replaceTop(() => Zon.UI.stageUIState.label.bottom + Zon.UI.stageUIState.height * 0.01);
+                d.replaceWidth(() => d.parent.width - (d.left - stageSelectBorder) * 2, { d });
+                d.replaceHeight(() => d.parent.height * 0.05, { d });
             }
         });
     }
